44561fdc29de2232f60de3783adedf15c2a8c7ef
faizanlaflames <fhussai5@asu.edu>
2025-02-09 21:50:30 -0800

    Added app files, tests, CI/CD stuff

---
+++ app.py
@@ -0,0 +1,8 @@
+# advanced application logic !
+def add(a, b):
+    return a + b
+
+if __name__ == "__main__":
+    print("Running the app...")
+    result = add(2, 3)
+    print(f"2 + 3 = {result}")
